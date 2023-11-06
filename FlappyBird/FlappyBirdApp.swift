ba4ffa6b0c15bbfb919d38bf2989111e0d6f9e0e
Denis Chernyatin <ya.chernyatin@gmail.com>
2023-11-06 20:07:48 +0300

    Initial Commit

---
+++ FlappyBird/FlappyBirdApp.swift
@@ -0,0 +1,17 @@
+//
+//  FlappyBirdApp.swift
+//  FlappyBird
+//
+//  Created by Денис Чернятин on 06.11.2023.
+//
+
+import SwiftUI
+
+@main
+struct FlappyBirdApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
